650be2a0873e93192d6de27aabc847af4947e83f
Kimberly Munoz <hi@kimberlymunoz.com>
2015-09-06 23:15:56 -0700

    🚙
    Adding Foundation files and porting over customizations

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp.task('default', function() {
-  
+gulp.task('sass', function () {
+  gulp.src('./scss/main.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(gulp.dest('./css'));
 });
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./scss/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass']);
